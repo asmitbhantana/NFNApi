3fd1c677153558d16205983647616302ebfc46a1
Asmit <bhantanasmit@gmail.com>
2020-12-03 18:48:13 +0545

    fix:User model removed

--- User/abstract.py
+++ User/abstract.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 
-class DateTimeEntity(models.Model):
-    date_created = models.DateTimeField(auto_now_add=True, auto_created=True)
-    date_updated = models.DateTimeField(auto_now=True, auto_created=True)
-
-    class Meta:
-        abstract = True
+# class DateTimeEntity(models.Model):
+#     date_created = models.DateTimeField(auto_now_add=True, auto_created=True)
+#     date_updated = models.DateTimeField(auto_now=True, auto_created=True)
+#
+#     class Meta:
+#         abstract = True
